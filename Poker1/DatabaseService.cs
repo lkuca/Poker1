d7754ec19e92fcf265bf6de74239b1d4d2aadf98
opilane <opilane@HP-CZC2349HV1>
2024-06-06 08:58:36 +0300

    sASsssS

---
+++ Poker1/DatabaseService.cs
@@ -0,0 +1,47 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker1
+{
+    public class DatabaseService
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public DatabaseService(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Player>().Wait();
+        }
+
+        public Task<List<Player>> GetPlayersAsync()
+        {
+            return _database.Table<Player>().ToListAsync();
+        }
+
+        public Task<Player> GetPlayerAsync(int id)
+        {
+            return _database.Table<Player>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SavePlayerAsync(Player player)
+        {
+            if (player.Id != 0)
+            {
+                return _database.UpdateAsync(player);
+            }
+            else
+            {
+                return _database.InsertAsync(player);
+            }
+        }
+
+        public Task<int> DeletePlayerAsync(Player player)
+        {
+            return _database.DeleteAsync(player);
+        }
+    }
+}
---
+++ Poker1/Player.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker1
+{
+    public class Player
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; } // Добавляем свойство Id
+        public string Name { get; set; }
+        public int Wins { get; set; }
+
+        public Player()
+        {
+            Name = string.Empty; // Initialize Name to an empty string
+            Wins = 0;
+        }
+    }
+}
